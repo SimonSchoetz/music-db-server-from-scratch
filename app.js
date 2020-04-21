c08cd7d0f6ef55bf12ce826443dd8828b9cf2bfe
Simon <simonsch.tz@gmail.com>
2020-04-21 14:37:58 +0200

    Set up basic infrastructure in app.js for forthcoming index and contact route files

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const createError = require("http-errors");
 
+const indexRoute = require();
+const contactsRoute = require();
+
 const port = process.env.PORT || 3000;
 
+app.use(express.json()) //translate json into readable data
+
+app.use("/", indexRoute);
+app.use("/contacts", contactsRoute);
 
 
+//Error Handler
 app.use((req, res, next) => {
     next(createError(404))
 });
