6bf151b399fe061611323b099a939ec34c157e1e
Simon <simonsch.tz@gmail.com>
2020-04-21 14:19:09 +0200

    Implement error handler and confirm with it, that server is working

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const createError = require("http-errors");
 
 const port = process.env.PORT || 3000;
+
+
+
+app.use((req, res, next) => {
+    next(createError(404))
+});
+
+app.use((err, req, res, next) => {
+    res.json({
+        status: err.status,
+        err: err.message
+    })
+})
 
 app.listen(port, () => console.log(`Server ist am been`));
