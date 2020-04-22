fe86c3586aadaa0d50cf8430e7f030ff311b151c
Simon <simonsch.tz@gmail.com>
2020-04-22 14:33:53 +0200

    Build up usersController based on musicController

---
+++ controllers/usersController.js
@@ -0,0 +1,27 @@
+const db = require("../models/db");
+
+exports.getUser = (req, res, next) => {
+    let user = db.get("users").value()
+    res.json({ success: true, user: user });
+};
+
+exports.getUserById = (req, res, next) => {
+    const { id } = req.params;
+    let user = db.get("users").find({ id })
+    res.json({ success: true, user: user })
+};
+
+exports.postUser = (req, res, next) => {
+    db.get("users").push(req.body).last().assign({ id: new Date().toString() }).write();
+};
+exports.putUser = (req, res, next) => {
+    const { id } = req.params;
+    const user = req.body;
+    db.get("users").find({ id }).assign(user).write();
+    res.json({ success: true, user: user });
+};
+exports.deleteUser = (req, res, next) => {
+    const { id } = req.params;
+    let user = db.get("users").remove({ id }).write();
+    res.json({ success: true, user: user })
+};
