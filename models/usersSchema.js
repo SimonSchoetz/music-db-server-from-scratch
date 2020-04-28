7ae7b516441ecba2dd918c273cf855cba5399a70
Simon <simonsch.tz@gmail.com>
2020-04-28 13:57:33 +0200

    Set usersSchema to only accept unique email addresses

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema(
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
         userName: { type: String, required: true },
-        email: { type: String, required: true },
+        email: { type: String, required: true, dropDups: true },
         pw: { type: String, required: true },
     }
 );
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,6 +1,6 @@
 const Route = require("express").Router();
 const { getUserById, getUser, postUser, putUser, deleteUser } = require("../controllers/usersController");
-const { validUserInputs } = require("./middleware/usersValidator");
+const { validUserInputs } = require("../middleware/usersValidator");
 
 Route.get("/", getUser);
 Route.post("/", validUserInputs(), postUser);
