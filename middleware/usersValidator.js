2fb5be2e45956f5c8ea2e7d494b813ef28f97f77
Simon <simonsch.tz@gmail.com>
2020-04-28 13:43:56 +0200

    Set up usersValidator

--- middleware/usersValidator.js
+++ middleware/usersValidator.js
@@ -2,6 +2,33 @@ const { body, validationResult } = require("express-validator");
 
 exports.validUserInputs = () => {
     return [
+        body("firstName")
+            .notEmpty()
+            .trim()
+            .escape()
+            .withMessage("Invalid first name."),
 
+        body("lastName")
+            .notEmpty()
+            .trim()
+            .escape()
+            .withMessage("Invalid last name."),
+
+        body("userName")
+            .notEmpty()
+            .trim()
+            .escape()
+            .withMessage("Invalid user name."),
+
+        body("email")
+            .isEmail()
+            .normalizeEmail()
+            .withMessage("Invalid email address."),
+
+        body("pw")
+            .notEmpty()
+            .trim()
+            .isLength({ min: 8 })
+            .withMessage("Invalid password.")
     ]
 }
