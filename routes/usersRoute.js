9042d481c19a7d547bcde52a4149b373996a29be
Simon <simonsch.tz@gmail.com>
2020-04-29 14:11:40 +0200

    Add /login route in usersRoute

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,9 +1,10 @@
 const Route = require("express").Router();
-const { getUserById, getUser, postUser, putUser, deleteUser } = require("../controllers/usersController");
+const { getUserById, getUser, postUser, putUser, deleteUser, login } = require("../controllers/usersController");
 const { validUserInputs } = require("../middleware/usersValidator");
 
 Route.get("/", getUser);
 Route.post("/", validUserInputs(), postUser);
+Route.post("/login", login)
 //Alternative syntax for practice purposes
 Route.route("/:id")
     .get(getUserById)
