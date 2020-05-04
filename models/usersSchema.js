718bb339649481b4658cd76164e98ba69a081c5c
Simon <simonsch.tz@gmail.com>
2020-05-04 14:44:46 +0200

    Copy the user methods we built at live coding into this project

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -22,4 +22,52 @@ const UserSchema = new Schema(
     }
 );
 
+
+UserSchema.methods.generateAuthToken = function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id }, "secretKey").toString(); //secretKey is a dummy for the admin pw
+    user.tokens.push({ token });
+    return token
+};
+
+//only show the following data to the user, the rest stays hidden
+UserSchema.methods.getPublicFields = function () {
+    const returnObject = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email.email,
+        _id: this._id
+    }
+    return returnObject
+};
+
+// compare pwInput with saved user.pw
+UserSchema.methods.checkPW = async function (pwInput) {
+    const user = this;
+    return await compare(pwInput, user.pw)
+}
+
+//in the save function in postUser is a built-in function called pre and post
+UserSchema.pre("save", async function (next) {
+    // if (!this.isModified("pw")) return next() //for the future when we implement function to change the pw
+    this.pw = await encrypt(this.pw);
+    next(); //next means in this case complete the process of storing the data
+})
+
+//Find user by token
+UserSchema.statics.findByToken = function (token) {
+    const User = this;
+    let decoded;
+    try {
+        decoded = jwt.verify(token, "secretKey")
+    } catch (err) {
+        return err
+    }
+
+    return User.findOne({
+        _id: decoded._id,
+        // "tokens.token": token
+    }).select("-pw -__v")
+};
+
 module.exports = mongoose.model("User", UserSchema);
