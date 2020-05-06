e8fda7a9fa1b0dcaa97f25b42ad32e4c7ef68e26
Simon <simonsch.tz@gmail.com>
2020-05-06 15:24:25 +0200

    Initiaize Music- and UsersList.js

---
+++ client/src/MusicList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MusicList() {
+    return (
+        <div>
+            <h2>List Of Music</h2>
+        </div>
+    )
+}
---
+++ client/src/UserList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function UserList() {
+    return (
+        <div>
+            <h2>List Of Users</h2>
+
+        </div>
+    )
+}
