d383a73481ce3849e2f61231e8e6a262aac6f492
Simon <simonsch.tz@gmail.com>
2020-05-06 16:03:51 +0200

    Add first routes and link elements in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import UserList from "./UserList";
 import MusicList from "./MusicList";
 import Error from "./Error404";
@@ -11,11 +11,16 @@ function App() {
       <div className="App">
         <header className="App-header">
           <h1>Busted Fingerz Music Database</h1>
+          <nav>
+            <Link to="/user">Music</Link>
+            <Link to="/user">My Account</Link>
+          </nav>
         </header>
+
       </div>
       <Switch>
-        <Route exact path="/music" component={MusicList} />
-        <Route exact path="/users" component={UserList} />
+        <Route exact path="/" component={MusicList} />
+        <Route exact path="/user" component={UserList} />
         <Route component={Error} />
       </Switch>
 
