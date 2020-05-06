62f592fa386d0e2bc41e05b5fc4ad33e497074e8
Simon <simonsch.tz@gmail.com>
2020-05-06 15:36:39 +0200

    Set up Error404.js as fallback

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import UserList from "./UserList";
+import MusicList from "./MusicList";
+import Error from "./Error404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Busted Fingerz Music Database</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <h1>Busted Fingerz Music Database</h1>
+        </header>
+      </div>
+      <Switch>
+        <Route exact path="/music" component={MusicList} />
+        <Route exact path="/users" component={UserList} />
+        <Route component={Error} />
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
