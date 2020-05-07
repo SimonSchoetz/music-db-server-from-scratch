5f779f00487401640bfe5be8c72f4d5c750b15ea
Simon <simonsch.tz@gmail.com>
2020-05-07 11:25:59 +0200

    Fetch data from localhost

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import UserList from "./UserList";
@@ -6,13 +6,30 @@ import MusicList from "./MusicList";
 import Error from "./Error404";
 
 function App() {
+
+
+
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    let serverData = await fetch("http://localhost:3000/music")
+    let convertedData = await serverData.json()
+    setData(convertedData)
+    console.log(convertedData.music)
+  }
+
+
+
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
           <h1>Busted Fingerz Music Database</h1>
           <nav>
-            <Link to="/user">Music</Link>
+            <Link to="/">Music</Link>
             <Link to="/user">My Account</Link>
           </nav>
         </header>
--- client/src/MusicList.js
+++ client/src/MusicList.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function MusicList() {
+
+
+
+
+
+    const renderLi = (el, i) => {
+
+        return (
+            <li key={i}>
+                <ul>
+                    <li><button onClick={() => console.log()}>click me</button>   </li>
+                    <li>Title: {}</li>
+                    <li>Artist: {}</li>
+                    <li>Label:{}</li>
+                    <li>Release Date:{}</li>
+                </ul>
+            </li>
+        )
+    };
+
     return (
         <div>
             <h2>List Of Music</h2>
+            <ul>
+                {renderLi()}
+            </ul>
         </div>
     )
-}
+};
