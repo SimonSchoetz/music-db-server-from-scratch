2e014ced42260645e4dae720d869db990b871483
Simon <simonsch.tz@gmail.com>
2020-05-11 16:56:12 +0200

    Add links to sign up and log in areas in AccountManager

--- client/src/AccountManager.js
+++ client/src/AccountManager.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function UserList(props) {
 
@@ -12,7 +13,7 @@ export default function UserList(props) {
 
 
     const renderLi = (userData) => {
-        if (userData.status === 403) return (<h2>Sign Up || Log In</h2>)
+        if (userData.status === 403) return (<h2> <Link to="/user/signup">Sign Up</Link>  || <Link to="/user/login">Log In</Link></h2>)
         if (!userData.users) return null; //Because first time the code is running, userData will be an empty array
 
 
