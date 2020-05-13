76293dc6d61aec9e10bb5097583b509045e4566d
Simon <simonsch.tz@gmail.com>
2020-05-13 19:42:14 +0200

    Show data on music detail

--- client/src/MusicDetail.js
+++ client/src/MusicDetail.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function MusicDetail(props) {
     const [musicData, setMusicData] = useState([]);
@@ -9,11 +10,32 @@ export default function MusicDetail(props) {
             .then(res => res.json())
             .then(data => setMusicData(data.music))
     }, [])
-
     console.log(musicData)
+
+    const renderLi = (musicData) => {
+        if (musicData.status === 404) return (<h2>Error 404, something went wrong</h2>)
+        if (musicData.length === 0) return null;
+        return (
+            <ul>
+                <li>{musicData.title}</li>
+                <li>{musicData.artist}</li>
+                <li>{musicData.album}</li>
+                <li>{musicData.label}</li>
+                <li>{musicData.release.substring(0, 10)}</li>
+                <li><Link to={musicData.link}>Purchase/Download </Link></li>
+                <li><img src={musicData.img} /> </li>
+
+            </ul>
+        )
+
+    };
+
+
+
     return (
         <div>
             <h2>This is music Detail</h2>
+            {renderLi(musicData)}
         </div>
     )
 }
--- client/src/MusicList.js
+++ client/src/MusicList.js
@@ -15,8 +15,7 @@ export default function MusicList() {
     //list item construction
     const renderLi = (musicData) => {
         if (musicData.status === 404) return (<h2>Error 404, something went wrong</h2>)
-        if (!musicData) return null; //Because first time the code is running, musicData will be an empty array
-        // console.log(musicData.music[0].release.getDay())
+        if (musicData.length === 0) return null; //Because first time the code is running, musicData will be an empty array
         return musicData.map((el, i) => (
 
 
